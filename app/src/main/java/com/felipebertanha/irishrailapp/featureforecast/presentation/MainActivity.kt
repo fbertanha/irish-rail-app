d281ceb5b6a1b40813a98b7bb50f8f2d7963cf3d
Felipe Bertanha <fbertanha.dev@gmail.com>
2022-10-21 16:23:13 +0100

    Initial commit

---
+++ app/src/main/java/com/felipebertanha/irishrailapp/featureforecast/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.felipebertanha.irishrailapp.featureforecast.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.felipebertanha.irishrailapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
