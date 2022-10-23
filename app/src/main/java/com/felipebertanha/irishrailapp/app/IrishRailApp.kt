1d14df50290f5fafeecf6172a9fc66823e40efb5
Felipe Bertanha <fbertanha.dev@gmail.com>
2022-10-23 14:53:33 +0100

    Manifest and icons updated

---
+++ app/src/main/java/com/felipebertanha/irishrailapp/app/IrishRailApp.kt
@@ -0,0 +1,11 @@
+package com.felipebertanha.irishrailapp.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by felipebertanha on 21/October/2022
+ */
+
+@HiltAndroidApp
+class IrishRailApp : Application()
